a88ed6371162615b472f3c375fe2bd3fadf46d2c
Codyde <codydearkland@gmail.com>
2019-06-13 16:21:50 -0700

    Update renameavm.py

--- Actions/renameavm.py
+++ Actions/renameavm.py
@@ -11,7 +11,7 @@ def handler(context, inputs):
     old_name = inputs["resourceNames"][0]
     new_name = inputs["customProperties"]["newName"]
     #Brand new comment
-
+    #Oh Jonny boyyyyy
     outputs = {}
     outputs["resourceNames"] = inputs["resourceNames"]
     outputs["resourceNames"][0] = new_name
