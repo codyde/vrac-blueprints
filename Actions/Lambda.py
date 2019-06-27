dd274bc6bc8c128abfdde370b931ba5f566195ba
Codyde <codydearkland@gmail.com>
2019-06-27 13:45:03 -0700

    Action Version: 1

---
+++ Actions/Lambda.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+def handler(context, inputs):
+    name = inputs['resourceNames'][0]
+    os = inputs['customProperties']['image']
+    reqid = inputs['requestId']
+    print(name)
+    data = {"text":"Your VM named "+name+" has completed\n The Operating System is "+os+"\n For future API calls your request ID is "+reqid}
+    url = "https://hooks.slack.com/services/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"
+    r = requests.post(url, data = json.dumps(data), verify=False)
+    if r.status_code == 200:
+        print("success")
+        outputs = {"statusCode":r.status_code}
+        return outputs
+    else:
+        print("Failure")
+        outputs = {"statusCode":r.status_code}
+        return outputs
